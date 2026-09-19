886c3f98be2ece954b6291c1d8ad42952dfb7116
Patel enterprises <116709112+bhanuprakashchII67@users.noreply.github.com>
2026-09-19 16:58:42 +0530

    Prevent stale Manager JavaScript cache

--- sw.js
+++ sw.js
@@ -1,41 +1,4 @@
-const CACHE_NAME = 'cleancore-manager-v3.6.3';
-
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  const request = event.request;
-
-  if (request.mode === 'navigate') {
-    event.respondWith(
-      fetch(request, { cache: 'no-store' })
-        .catch(() => caches.match(request))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(request)
-      .then(response => {
-        if (response.ok && new URL(request.url).origin === self.location.origin) {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-        }
-        return response;
-      })
-      .catch(() => caches.match(request))
-  );
-});
+const CACHE_NAME='cleancore-manager-v3.7.6';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{const r=e.request,u=new URL(r.url);if(r.mode==='navigate'||/\/(app|index|sw)\.js$/.test(u.pathname)){e.respondWith(fetch(r,{cache:'no-store'}).catch(()=>caches.match(r)));return;}e.respondWith(fetch(r).then(res=>{if(res.ok&&u.origin===self.location.origin){const c=res.clone();caches.open(CACHE_NAME).then(x=>x.put(r,c));}return res;}).catch(()=>caches.match(r)));});
