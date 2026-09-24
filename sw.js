d9687c047f10d50fa3b1b0695d8d9d97a90ec6d9
cleancore <116709112+cleancore01@users.noreply.github.com>
2026-09-25 00:07:03 +0530

    Refresh Manager service worker after numeric input fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cleancore-manager-v3.8.59-r1';
+const CACHE_NAME='cleancore-manager-v3.8.60-r1';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
