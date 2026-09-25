e1aedf1dc1889383f0f371b0ceb1b5a78ea3f0ac
cleancore <116709112+cleancore01@users.noreply.github.com>
2026-09-25 18:45:09 +0530

    Refresh service worker after Manager boot fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='cleancore-manager-v3.8.65-r1';
+const CACHE_NAME='cleancore-manager-v3.8.66-r2';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
